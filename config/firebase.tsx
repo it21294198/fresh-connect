99e8cccef1fb2c25373414fcb4a133b8b8034922
it21294198 <it21294198@my.sliit.lk>
2023-09-30 10:20:52 +0530

    initial commit

---
+++ config/firebase.tsx
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import {
+  EXPO_PUBLIC_API_KEY,
+  EXPO_PUBLIC_AUTH_DOMAIN,
+  EXPO_PUBLIC_PROJECT_ID,
+  EXPO_PUBLIC_STORAGE_BUCKET,
+  EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  EXPO_PUBLIC_APP_ID,
+} from "@env";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: EXPO_PUBLIC_API_KEY,
+  authDomain: EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: EXPO_PUBLIC_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const fireStore = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+
+export { fireStore, storage, auth };
