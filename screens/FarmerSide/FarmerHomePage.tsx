e198be9d87a6ec363b7bce255c87955ba7d13f37
it21294198 <it21294198@my.sliit.lk>
2023-10-01 01:36:52 +0530

    add pages

---
+++ screens/FarmerSide/FarmerHomePage.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function FarmerHomePage() {
+  return (
+    <View>
+      <Text>FarmerHomePage</Text>
+    </View>
+  )
+}
---
+++ screens/FarmerSide/FarmerProfile.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function FarmerProfile() {
+  return (
+    <View>
+      <Text>FarmerProfile</Text>
+    </View>
+  )
+}
