567bc1c249f210946b5ad6b6cc041f90af4bcc57
it21294198 <it21294198@my.sliit.lk>
2023-09-30 10:20:52 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { t } from 'react-native-tailwindcss';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import NetInfo from '@react-native-community/netinfo';
+import { EXPO_PUBLIC_APP_ID } from '@env';
+
 export default function App() {
+
+  NetInfo.fetch().then(state => {
+      console.log('Connection type', state.type);
+      console.log('Is connected?', state.isConnected);
+    });
+
   return (
+    // here using react native styles
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* here using tailwind styles refer following*/}
+      {/* https://tvke.github.io/react-native-tailwindcss/directional.html */}
+      <Text style={[t.bgBlue500,t.fontBold]}>Open up App.tsx to start working on your app!</Text>
+      {/* this is how to use env values */}
+      <Text style={[t.bgRed400]}>Test env keys : <Text style={[t.bgBlue700]}>{process.env.EXPO_PUBLIC_APP_ID}</Text></Text>
+      {/* this is how use icons */}
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,22 @@
+## When you clone the project first run following commands and create some file
+
+### Move to folder
+```
+cd f [press tab key]
+```
+
+### Install npm
+```
+npm i
+```
+
+### create file name **.env.local** in root folder
+### Include the firebase config
+```
+  EXPO_PUBLIC_API_KEY = 
+  EXPO_PUBLIC_AUTH_DOMAIN = 
+  EXPO_PUBLIC_PROJECT_ID = 
+  EXPO_PUBLIC_STORAGE_BUCKET = 
+  EXPO_PUBLIC_MESSAGING_SENDER_ID = 
+  EXPO_PUBLIC_APP_ID = 
+```
