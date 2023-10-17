aeea0a5b57b2b486856ecc7a525a49c57049ebb9
it21294198 <it21294198@my.sliit.lk>
2023-10-17 18:17:41 +0530

    remove tailwind

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { StyleSheet,View,Text } from 'react-native';
-import { t } from 'react-native-tailwindcss';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import NetInfo from '@react-native-community/netinfo';
 import { NavigationContainer } from '@react-navigation/native';
@@ -38,31 +37,8 @@ export function Main() {
 export default function App(){
     return(
       <Provider store={store}>
-        <Main/>
+      <View><Text>remove mytailwind</Text></View>
+        {/* <Main/> */}
       </Provider>
   )
 }
-
-// Test Area
-// export default function App() {
-//   return (
-//     // here using react native styles
-//         <View style={styles.container}>
-//           {/* here using tailwind styles refer following*/}
-//           {/* https://tvke.github.io/react-native-tailwindcss/directional.html */}
-//           <Text style={[t.bgBlue500,t.fontBold]}>Open up App.tsx to start working on your app!</Text>
-//           {/* this is how to use env values */}
-//           <Text style={[t.bgRed400]}>Test env keys : <Text style={[t.bgBlue700]}>{process.env.EXPO_PUBLIC_APP_ID}</Text></Text>
-//           {/* this is how use icons */}
-//           <Ionicons name="md-checkmark-circle" size={32} color="green" />
-//         </View>
-//       );
-//     }
-// const styles = StyleSheet.create({
-//       container: {
-//         flex: 1,
-//         backgroundColor: '#fff',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//       },
-//     });
