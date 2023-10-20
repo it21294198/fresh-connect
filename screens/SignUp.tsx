73127458ae35177764849b2fc5fa6e2f39167d2f
it21294198 <it21294198@my.sliit.lk>
2023-10-21 00:43:09 +0530

    profile created

--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
   },
   bottom:{
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: '#45A053',
     borderWidth: 0,
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
