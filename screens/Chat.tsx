24ecfb25d56d340fb2d66b9da2f2c01ed98a213c
IT21292422 <it21292422@my.sliit.lk>
2023-10-20 17:04:19 +0530

    chat init

--- screens/Chat.tsx
+++ screens/Chat.tsx
@@ -1,10 +1,33 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
 
-export default function Chat() {
+export default function Chat({ route, navigation }: any) {
+  const [messages, setMessages] = useState([])
+  const { user,name } = route.params
+  const data = [
+    {
+      _id: 0,
+      text: 'New Chat Created',
+      createdAt: new Date().getTime(),
+      system: true
+    },
+    {
+      _id: 1,
+      text: 'Hello!',
+      createdAt: new Date().getTime(),
+      user: user
+    }
+  ]
+
+  const handleSend: any = (newMessage = []) => {
+    setMessages(GiftedChat.append(messages, newMessage))
+  }
+
   return (
     <View>
-      <Text>Chat</Text>
+      <Text>This is {name}</Text>
+      <GiftedChat messages={messages} onSend={newMessage => handleSend(newMessage)} user={{ _id: 1 }} />
     </View>
   )
 }
