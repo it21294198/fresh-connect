4b9575b42e0e3c1b34c8bc658f4f684834693a31
chatwick <c.wick400@gmail.com>
2023-10-22 17:02:46 +0530

    map  setup

--- components/headers/MapDisplayHeader.tsx
+++ components/headers/MapDisplayHeader.tsx
@@ -27,21 +27,32 @@ export const MapDisplayHeader = ({ navigation }: any) =>
                                     <Image source={customHamburger} style={{ width: 19, height: 21, resizeMode: 'contain', }} />
                                 }>
                             </Button>
-                        }> <Button
-                            borderColor='black' 
-                            borderWidth={1} 
-                            bg='transparent' 
-                            color='black' 
-                            alignSelf="center"
-                            style={{marginHorizontal: 50, paddingVertical:0, marginVertical:0}}
+                        }>
+                        <Button
+                            borderColor='black'
+                            borderWidth={1}
+                            alignSelf='center'
+                            bg='transparent'
+                            color='black'
                             rounded={25}
-                            prefix={
-                                <Image source={searchIcon} style={{ width: 19, height: 21, resizeMode: 'contain', paddingHorizontal:20, marginVertical:0 }} />
-                            }
-                            >{text}</Button>
+                            w={200}
+                            prefix={<Image source={searchIcon} style={{width: 19, height:20, resizeMode: 'contain', paddingHorizontal: 20}}></Image>}
+                        >Search</Button>
                     </Header>
                 </Div>
             </Div>
         </>
     )
 }
+
+<Button
+    borderColor='black'
+    borderWidth={1}
+    bg='transparent'
+    color='black'
+    alignSelf="center"
+    style={{ marginHorizontal: 50, paddingVertical: 0, marginVertical: 0 }}
+    rounded={25}
+    prefix={
+        <Image source={searchIcon} style={{ width: 19, height: 21, resizeMode: 'contain', paddingHorizontal: 20, marginVertical: 0 }} />
+    }></Button>
