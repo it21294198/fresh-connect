8aeb77d4c56b54491f5624daadcfeed0695ad381
chatwick <c.wick400@gmail.com>
2023-10-22 17:02:46 +0530

    map  setup

--- screens/CustomerSide/ShopMapDisplay.tsx
+++ screens/CustomerSide/ShopMapDisplay.tsx
@@ -1,20 +1,13 @@
-import { View, Text, ScrollView,} from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, StyleSheet, } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { fireStore } from '../../config/firebase';
-import
-{
-  collection,
-  getDocs,
-  doc,
-  getDoc
-} from 'firebase/firestore';
+import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import { Div, Button, Header, Icon } from 'react-native-magnus';
 import { MapDisplayHeader } from '../../components/headers/MapDisplayHeader';
 import { CommonHeader } from '../../components/headers/CommonHeader';
+import MapView from 'react-native-maps';
 
-
-
-export default function ShopMapDisplay({ navigation })
+export default function ShopMapDisplay({ navigation }: any)
 {
 
   const shopColRef = collection(fireStore, 'shops');
@@ -22,8 +15,20 @@ export default function ShopMapDisplay({ navigation })
 
   return (
     <Div>
-      {/* <MapDisplayHeader navigation={navigation} /> */}
-      <CommonHeader navigation={navigation} title='lorem ipsum dollar lament' headerRight={false} />
+      <MapDisplayHeader navigation={navigation} />
+      <Div>
+        <MapView style={styles.container} />
+      </Div>
     </Div>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
