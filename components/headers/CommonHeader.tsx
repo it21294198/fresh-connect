e136fb8c8d263788ba280ef624ca11c413325897
chatwick <c.wick400@gmail.com>
2023-10-22 14:59:03 +0530

    fonts and icons

---
+++ components/headers/CommonHeader.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Div, Button, Header, Icon } from 'react-native-magnus';
+import customHamburger from "../../assets/customHamburger.png"
+
+export const CommonHeader = ({navigation}) =>
+{
+    const handleDrawer = () =>
+    {
+        navigation.toggleDrawer(); // Function to toggle the drawer
+    };
+    
+    return (
+        <>
+            <Div>
+                <Div >
+                    <Header flex={1} flexDir='row'
+                        p="lg"
+                        borderBottomWidth={1}
+                        borderBottomColor="gray200"
+                        alignment="center"
+                        prefix={
+                            <Button bg="transparent" onPress={handleDrawer}
+                                prefix={
+                                    <Image source={customHamburger} style={{ width: 19, height: 21, resizeMode: 'contain', }} />
+                                }>
+                            </Button>
+                        }> <Button
+                            borderColor='black'
+                            borderWidth={1}
+                            bg='transparent'
+                            color='black'
+                            alignSelf="center"
+                            style={{ marginHorizontal: 50, paddingVertical: 0, marginVertical: 0 }}
+                            rounded={25}
+                            prefix={
+                                <Image source={searchIcon} style={{ width: 19, height: 21, resizeMode: 'contain', paddingHorizontal: 20, marginVertical: 0 }} />
+                            }
+                        >Search</Button>
+                    </Header>
+                </Div>
+            </Div>
+        </>
+    )
+}
--- screens/CustomerSide/ShopMapDisplay.tsx
+++ screens/CustomerSide/ShopMapDisplay.tsx
@@ -1,66 +1,24 @@
-import { View, Text, ScrollView, Button } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { fireStore } from '../../config/firebase';
-import {
+import
+{
   collection,
   getDocs,
-  DocumentData,
-  QueryDocumentSnapshot,
   doc,
-  deleteDoc,
+  getDoc
 } from 'firebase/firestore';
+import { Div, Button, Header, Icon } from 'react-native-magnus';
+import { MapDisplayHeader } from '../../components/headers/MapDisplayHeader';
 
-interface ShopData {
-  id: string;
-  text: string;
-  // Add other fields as needed
-}
 
-export default function ShopMapDisplay() {
-  const [data, setData] = useState<ShopData[]>([]);
+export default function ShopMapDisplay({navigation})
+{
 
-  useEffect(() => {
-    fetchDataFromFirestore();
-  }, []);
+  const shopColRef = collection(fireStore, 'shops');
 
-  const fetchDataFromFirestore = async () => {
-    try {
-      const querySnapshot = await getDocs(collection(fireStore, 'text'));
-      const items: any = querySnapshot.docs.map(
-        (doc: QueryDocumentSnapshot<DocumentData>) => ({
-          id: doc.id,
-          ...doc.data(),
-        })
-      );
-      setData(items);
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
-  };
-
-  const handleDelete = async (itemId: string) => {
-    try {
-      await deleteDoc(doc(fireStore, 'text', itemId));
-      fetchDataFromFirestore();
-    } catch (error) {
-      console.error('Error deleting item: ', error);
-    }
-  };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <ScrollView>
-        {data.map((item) => (
-          <View key={item.id}>
-            <Text>{item.text}</Text>
-            <Button
-              title="Delete"
-              onPress={() => handleDelete(item.id)}
-              color="red"
-            />
-          </View>
-        ))}
-      </ScrollView>
-    </View>
+    <MapDisplayHeader navigation={navigation}/>
   );
 }
